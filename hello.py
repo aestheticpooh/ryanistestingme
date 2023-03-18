d10a4244cdb39d999e34d41829a3b16b6651652d
Cassie J Kwong <aestheticpooh@Cassies-MacBook-Pro.local>
2023-03-17 22:48:57 -0700

    add back to branch

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 print("hello malnutrition boy :) heheh")
+
+"""ls - list, 
+cd - command, 
+1- 
+git add . - stage it
+2-
+git commit -m "write message here",
+3-
+git push
+
+--
+git fetch - syncs with whatever changes I make on github
+git branch - shows you what branches i have
+
+git branch - lists the branch
+git checkout -b backend = this creates a new branch
+"""
