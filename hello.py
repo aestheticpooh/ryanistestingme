5b5db8b0a652b9bb2ab1fbd30c6936b4cea4fcb0
Cassie J Kwong <aestheticpooh@Cassies-MacBook-Pro.local>
2023-03-17 23:20:29 -0700

    wedncioswnlcs

--- hello.py
+++ hello.py
@@ -1,22 +1,24 @@
+print("hello malnutrition boy :) heheh")
 
-dheqwuijdknklew k smtpd
 """ls - list, 
 cd - command, 
 1- 
-njakln cklxn lsdkjcslkcswdl;c;lsdfcvsdcvfevf
+git add . - stage it
+2-
+git commit -m "write message here",
+3-
+git push
+
+--
+git fetch - syncs with whatever changes I make on github
+git branch - shows you what branches i have
+
+git branch - lists the branch
+git checkout -b backend = this creates a new branch
 
 git checkout main
 git merge backend
-
-git checkout test = this is the new branch i made - test
 """
 
-for _ in ranjisjdf70234ge(5):
-    print("bruh")sanjckanslkcmsdlkmcksd
-
-    qwdnkjqlwmsldwsd
-
-
-    //sdncxaslcnkad
-
-
+for _ in range(5):
+    print("bruh")
