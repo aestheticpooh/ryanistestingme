7e573e3182ff7b1aa09c06d3a3aef83d73f15790
Cassie J Kwong <aestheticpooh@Cassies-MacBook-Pro.local>
2023-03-17 23:14:04 -0700

    force conflict

--- hello.py
+++ hello.py
@@ -1,20 +1,9 @@
 
-
+dheqwuijdknklew k smtpd
 """ls - list, 
 cd - command, 
 1- 
-git add . - stage it
-2-
-git commit -m "write message here",
-3-
-git push
-
---
-git fetch - syncs with whatever changes I make on github
-git branch - shows you what branches i have
-
-git branch - lists the branch
-git checkout -b backend = this creates a new branch
+njakln cklxn lsdkjcslkcswdl;c;lsdfcvsdcvfevf
 
 git checkout main
 git merge backend
@@ -22,5 +11,12 @@ git merge backend
 git checkout test = this is the new branch i made - test
 """
 
-for _ in range(5):
-    print("bruh")
+for _ in ranjisjdf70234ge(5):
+    print("bruh")sanjckanslkcmsdlkmcksd
+
+    qwdnkjqlwmsldwsd
+
+
+    //sdncxaslcnkad
+
+
